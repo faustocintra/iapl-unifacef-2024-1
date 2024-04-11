82342ad2300afb6f7fbaf4927c5ef0ad3fc9dcc0
Lucas Henrique <lucas.barbosa3680@gmail.com>
2024-04-11 20:37:39 -0300

    (11/04) Navegação e tela de login no front-end

---
+++ front-end/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Typography from '@mui/material/Typography';
+
+export default function HomePage() {
+  return (
+    <div>
+      <Typography variant="h2" gutterBottom>
+        Projeto Integração de Aplicações
+      </Typography>
+      <p>Bem-vindo(a) ao projeto Integração de Aplicações</p>
+    </div>
+  );
+}
