f84f6c328afc61c9a156c16bd1416076eb176561
Yuri Fernandes <yuricaparelliofc@gmail.com>
2024-04-23 20:33:44 -0300

    create routes

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -27,8 +27,12 @@ app.use(auth)
 /*************************************************
  * ROTAS
  *************************************************/
-
 import usersRouter from './routes/users.js'
+
+import carsRouter from './routes/cars.js'
+
 app.use('/users', usersRouter)
+
+app.use('/cars', carsRouter)
 
 export default app;
