a1883edde423e586c4608e161d4db5a4d33d7f0c
miguell-gomes@hotmail.com <miguell-gomes@hotmail.com>
2024-04-23 20:04:41 -0300

    Criação CRUD Cars

---
+++ back-end/prisma/migrations/20240423222546_create_cars/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Cars" (
+    "id" SERIAL NOT NULL,
+    "brand" TEXT NOT NULL,
+    "model" TEXT NOT NULL,
+    "owner_name" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+    "year" INTEGER NOT NULL,
+    "imported" BOOLEAN NOT NULL,
+    "plates" TEXT NOT NULL,
+
+    CONSTRAINT "Cars_pkey" PRIMARY KEY ("id")
+);
