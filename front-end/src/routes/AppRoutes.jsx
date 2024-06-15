ff2f48290f16915fb5115483901680c1fd5b88e0
Arthur H <arthurhenriquep2@gmail.com>
2024-06-14 22:13:17 -0300

    (02/05) Listagem de usuários protegida no front-end

--- front-end/src/routes/AppRoutes.jsx
+++ front-end/src/routes/AppRoutes.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import UserList from '../pages/UserList'
 
 import HomePage from '../pages/HomePage'
 import LoginPage from '../pages/LoginPage'
@@ -8,5 +9,6 @@ export default function AppRoutes() {
   return <Routes>
     <Route path="/" element={ <HomePage /> } />
     <Route path="/login" element = { <LoginPage /> } />
+    <Route path="/users" element = { <UserList /> } />
   </Routes>
 }
