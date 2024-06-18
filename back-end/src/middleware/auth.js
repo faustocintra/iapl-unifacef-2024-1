11810f55b3df77520ffbf89b390091a4c3f185b5
Arthur Henrique de Paula <81483650+arthurhenriquedepaula@users.noreply.github.com>
2024-06-18 20:41:38 -0300

    prova-arthurhenrique-23638

--- back-end/src/middleware/auth.js
+++ back-end/src/middleware/auth.js
@@ -1,103 +1,105 @@
-import Cryptr from 'cryptr'
-import prisma from '../database/client.js'
+import Cryptr from 'cryptr';
+import prisma from '../database/client.js';
+
+/*
+  Middleware de Autenticação por Sessão
+
+  Este middleware protege rotas sensíveis verificando a presença e validade da sessid.
+  A sessid é descriptografada para recuperar informações da sessão do usuário armazenadas no banco de dados.
+
+  Fluxo:
+  1. Verifica se a rota atual está na lista de rotas que não necessitam de autenticação.
+  2. Tenta encontrar a sessid em um cookie específico.
+  3. Se não encontrada no cookie, procura no header 'Authorization'.
+  4. Descriptografa a sessid usando a chave secreta definida no ambiente.
+  5. Busca as informações da sessão no banco de dados utilizando a sessid.
+  6. Verifica se a sessão é válida (não expirou).
+  7. Se a sessão é válida, armazena as informações do usuário recuperadas em 'req.authUser' para uso posterior.
+  8. Caso contrário, retorna um erro 403 (Forbidden) indicando falha na autenticação.
+
+  Notas:
+  - A sessid é enviada pelo cliente em cada requisição e usada para manter o estado de autenticação.
+*/
 
 export default async function(req, res, next) {
 
-  // As rotas que eventualmente não necessitarem
-  // de autenticação devem ser colocadas no
-  // objeto abaixo
+  // Rotas que não necessitam de autenticação
   const bypassRoutes = [
     { url: '/users/login', method: 'POST' },
     { url: '/users', method: 'POST' }
-  ]
+  ];
 
-  // Verifica se a rota atual está nas exceções
-  // de bypassRoutes. Caso esteja, passa para o
-  // próximo middleware sem verificar a autenticação
+  // Verifica se a rota atual está nas exceções de bypassRoutes
   for(let route of bypassRoutes) {
     if(route.url === req.url && route.method === req.method) {
-      console.log(`Rota ${route.url}, método ${route.method} não autenticados por exceção`)
-      next()
-      return
+      console.log(`Rota ${route.url}, método ${route.method} não autenticados por exceção`);
+      next();
+      return;
     }
   }
+  
+  // Procura a sessid em um cookie
+  let cryptoSessid = req.cookies[process.env.AUTH_COOKIE_NAME];
 
-  // Para todas as demais rotas, é necessário que a sessid tenha
-  // sido enviada em um cookie ou no cabeçalho Authorization
+  // Se a sessid não foi encontrada no cookie, procura no header de autorização
+  if(!cryptoSessid) {
+    const authHeader = req.headers['authorization'];
 
-  let cryptoSessid = null
-
-  // console.log({ COOKIE: req.cookies[process.env.AUTH_COOKIE_NAME] })
-
-  // 1. PROCURA A SESSID EM UM COOKIE
-  cryptoSessid = req.cookies[process.env.AUTH_COOKIE_NAME]
-
-  // 2. SE A SESSID NÃO FOI ENCONTRADA NO COOKIE, PROCURA NO HEADER
-  // DE AUTORIZAÇÃO
-  if(! cryptoSessid) {
-    const authHeader = req.headers['authorization']
-
-    // O header não existe, a sessid não foi passada:
-    // HTTP 403: Forbidden
-    if(! authHeader) {
-      console.error('ERRO: não autenticado por falta de cookie ou cabeçalho de autorização')
-      return res.status(403).end()
+    // Se o header de autorização não existe
+    if(!authHeader) {
+      console.error('ERRO: não autenticado por falta de cookie ou cabeçalho de autorização');
+      return res.status(403).end();
     }
-
-    // O header Authorization é enviado como uma string
-    // Bearer: XXXX
-    // onde XXXX é o token. Portanto, para extrair o token,
-    // precisamos recortar a string no ponto onde há um espaço
-    // e pegar somente a a segunda parte
-    const [ , _token] = authHeader.split(' ')
-
-    cryptoSessid = _token
+  
+    // O header Authorization é enviado como uma string 'Bearer: XXXX'
+    // Extrai apenas o token (XXXX)
+    const [ , _token] = authHeader.split(' ');
+    
+    cryptoSessid = _token;
   }
- " // VALIDA A SESSSID
-  let sessid
 
-  // Tenta descriptografar a sessid
+  // Descriptografa a sessid
+  let sessid;
   try {
-    const cryptr = new Cryptr(process.env.TOKEN_SECRET)
-    sessid = cryptr.decrypt(cryptoSessid)
+    const cryptr = new Cryptr(process.env.TOKEN_SECRET);
+    sessid = cryptr.decrypt(cryptoSessid);
   }
   catch {
     // Caso ocorra algum erro com a decriptografia da sessid,
     // enviamos HTTP 403: Forbidden
-    console.error('ERRO: não autenticado por falha na decodificação da sessid')
-    return res.status(403).end()
+    console.error('ERRO: não autenticado por falha na decodificação da sessid');
+    return res.status(403).end();
   }
 
-  // Buscamos as informações da sessão no banco de dados
-  let session
+  // Busca as informações da sessão no banco de dados
+  let session;
   try {
     session = await prisma.session.findUniqueOrThrow({
       where: { sessid },
       include: { user: true } // Faz "join" com a tabela de usuários
-    })
+    });
   }
   catch {
     // Caso ocorra algum erro com a recuperação das informações da sessão,
     // enviamos HTTP 403: Forbidden
-    console.error('ERRO: não autenticado por erro na recuperação das informações da sessão')
-    return res.status(403).end()
+    console.error('ERRO: não autenticado por erro na recuperação das informações da sessão');
+    return res.status(403).end();
   }
 
   // Verifica se a sessão é válida (não está expirada)
-  const now = new Date()    // Data/hora atuais
-  if(now.getTime() - session.start_time.getTime() > 
-    Number(process.env.SESSION_DURATION)) {
-
-    console.error('ERRO: não autenticado por sessão expirada.')
-    return res.status(403).end()
+  const now = new Date();    // Data/hora atuais
+  if(now.getTime() - session.start_time.getTime() > Number(process.env.SESSION_DURATION)) {
+    console.error('ERRO: não autenticado por sessão expirada.');
+    return res.status(403).end();
   }
 
-  // Sessão OK, armazenamos os dados do usuário recuperados junto com a
-  // sessão em req.authUser para posterior utilização
-  // Obs.: tomamos o cuidado de apagar o campo password
-  if(session.user?.password) delete session.user?.password
-  req.authUser = session.user
+  // Sessão válida, armazena os dados do usuário recuperados
+  // em req.authUser para uso posterior
+  // Obs.: o campo 'password' é removido por questões de segurança
+  if(session.user?.password) delete session.user?.password;
+  req.authUser = session.user;
 
   // Continua para a rota normal
-  next()
+  next();
+
 }
--- back-end/src/middleware/auth.jwt.js
+++ back-end/src/middleware/auth.jwt.js
@@ -1,77 +1,77 @@
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
+
+/*
+  Middleware de Autenticação JWT
+
+  Este middleware protege rotas sensíveis verificando a presença e validade de um token JWT.
+  Se o token é válido, o usuário está autenticado e pode acessar a rota protegida.
+
+  Fluxo:
+  1. Verifica se a rota atual está na lista de rotas que não necessitam de autenticação.
+  2. Tenta encontrar o token JWT em um cookie específico.
+  3. Se não encontrado no cookie, procura no header 'Authorization'.
+  4. Valida o token JWT utilizando a chave secreta definida no ambiente.
+  5. Se o token é válido, adiciona as informações do usuário autenticado no objeto 'req' para uso futuro.
+  6. Caso contrário, retorna um erro 403 (Forbidden) indicando falha na autenticação.
+
+  Notas:
+  - O token JWT é emitido durante o login do usuário e deve ser enviado pelo cliente em cada requisição subsequente.
+*/
 
 export default function(req, res, next) {
 
-  // As rotas que eventualmente não necessitarem
-  // de autenticação devem ser colocadas no
-  // objeto abaixo
+  // Rotas que não necessitam de autenticação
   const bypassRoutes = [
     { url: '/users/login', method: 'POST' },
     { url: '/users', method: 'POST' }
-  ]
+  ];
 
-  // Verifica se a rota atual está nas exceções
-  // de bypassRoutes. Caso esteja, passa para o
-  // próximo middleware sem verificar a autenticação
+  // Verifica se a rota atual está nas exceções de bypassRoutes
   for(let route of bypassRoutes) {
     if(route.url === req.url && route.method === req.method) {
-      console.log(`Rota ${route.url}, método ${route.method} não autenticados por exceção`)
-      next()
-      return
+      console.log(`Rota ${route.url}, método ${route.method} não autenticados por exceção`);
+      next();
+      return;
     }
   }
+  
+  // Inicialmente, tenta encontrar o token JWT em um cookie
+  let token = req.cookies[process.env.AUTH_COOKIE_NAME];
 
-  // Para todas as demais rotas, é necessário que o token tenha
-  // sido enviado em um cookie ou no cabeçalho Authorization
+  // Se não encontrou o token no cookie, procura no header de autorização
+  if(!token) {
+    const authHeader = req.headers['authorization'];
 
-  let token = null
-
-  console.log({ COOKIE: req.cookies[process.env.AUTH_COOKIE_NAME] })
-
-  // 1. PROCURA O TOKEN EM UM COOKIE
-  token = req.cookies[process.env.AUTH_COOKIE_NAME]
-
-  // 2. SE O TOKEN NÃO FOI ENCONTRADO NO COOKIE, PROCURA NO HEADER
-  // DE AUTORIZAÇÃO
-  if(! token) {
-    const authHeader = req.headers['authorization']
-
-    // O header não existe, o token não foi passado:
-    // HTTP 403: Forbidden
-    if(! authHeader) {
-      console.error('ERRO: não autenticado por falta de cookie ou cabeçalho de autorização')
-      return res.status(403).end()
+    // Se o header de autorização não existe
+    if(!authHeader) {
+      console.error('ERRO: não autenticado por falta de cookie ou cabeçalho de autorização');
+      return res.status(403).end();
     }
-
-    // O header Authorization é enviado como uma string
-    // Bearer: XXXX
-    // onde XXXX é o token. Portanto, para extrair o token,
-    // precisamos recortar a string no ponto onde há um espaço
-    // e pegar somente a a segunda parte
-    const [ , _token] = authHeader.split(' ')
-
-    token = _token
+  
+    // O header Authorization é enviado como uma string 'Bearer: XXXX'
+    // Extrai apenas o token (XXXX)
+    const [ , _token] = authHeader.split(' ');
+    
+    token = _token;
   }
 
-  // Valida o token
+  // Valida o token JWT
   jwt.verify(token, process.env.TOKEN_SECRET, (error, user) => {
 
-    // Token inválido ou expirado
-    // HTTP 403: Forbidden
+    // Se o token é inválido ou expirado
     if(error) {
-      console.error('ERRO: token inválido ou expirado')
-      return res.status(403).end()
+      console.error('ERRO: token inválido ou expirado');
+      return res.status(403).end();
     }
 
     /*
-      Se chegamos até aqui, o token está OK e temos as informações
-      do usuário logado no parâmetro 'user'. Vamos guardá-lo no 'req'
-      para futura utilização
+      Se chegamos até aqui, o token está válido e 'user' contém as informações
+      do usuário autenticado. Armazena essas informações no objeto 'req' para uso posterior.
     */
-    req.authUser = user
-
+    req.authUser = user;
+    
     // Continua para a rota normal
-    next()
-  })
+    next();
+  });
 
 }
